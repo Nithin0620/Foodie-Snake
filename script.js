a5da44a0d88c0c1fbd77f7713ae83b37af5e0049
Nithin0620 <nithin30302@gmail.com>
2025-04-12 19:04:44 +0530

    UI Updated

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const foodsound = new Audio('music/food.mp3');
 const gameOverSound = new Audio('music/gameOver.mp3');
 const moveSound = new Audio('music/move.mp3');
 const musicSound = new Audio('music/music.mp3');
-let speed = 5;
+let speed = 7;
 let score = 0;
 let lastPaintTime = 0;
 
